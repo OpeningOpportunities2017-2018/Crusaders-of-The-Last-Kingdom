a7ca8c22b101c8cf6bb1f942d4c160d63c7d386a
Cosmin Aftanase <cosmin.aftanase@yahoo.com>
2017-12-03 22:10:06 +0200

    Revert "Functie verificare abilitate valida"
    This reverts commit a7119a289e243d0c6efcbc100424bc3377f593b4.

--- Joc/Assets/Scripturi/Jucator.cs
+++ Joc/Assets/Scripturi/Jucator.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class Jucator : MonoBehaviour 
 {
-	/*
-	 * Abilitati - Fiecare abilitate posibila in joc
-	 * Momentan am considerat ca fiecare abilitate are o singura "pozitie" posibila in joc. Daca trebuie facute modificari, ele trebuie facute in clasa Abilitate si in functia PoateFolosiAbilitate
-	 */
-	public List<Abilitate> abilitati = new List<Abilitate> ();
 	void Start()
 	{
-	}
-	bool PoateFolosiAbilitatea(int pozitie,Abilitate abil)
-	{
-		return pozitie == abil.GetPozitie ();
+		
 	}
 }
 public class Clasa
 {
 	string nume;
-	List<Abilitate> abilspec = new List<Abilitate>();
-	public Clasa()
+	public List<Abilitate> abilitati = new List<Abilitate> ();
+	Clasa()
 	{
 		
 	}
@@ -29,21 +21,4 @@ public class Clasa
 public class Abilitate
 {
 	string nume;
-	int pozitie;
-	public void SetNume(string s)
-	{
-		nume = s;
-	}
-	public string GetNume()
-	{
-		return nume;
-	}
-	public void SetPozitie(int p)
-	{
-		pozitie = p;
-	}
-	public int GetPozitie()
-	{
-		return pozitie;
-	}
 }
