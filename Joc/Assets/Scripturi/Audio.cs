51938f870bdccd74198c5a4e190089b141e0fa16
burnfire123 <stefanescupetru@yahoo.com>
2017-12-08 19:08:27 +0200

    Camera+Audio
    Am facut ajustarea camerei si am facut un script de baza pentru Audio. Nu ne trebuie acum, dar poate ca ne va trebui mai incolo

---
+++ Joc/Assets/Scripturi/Audio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour 
+{
+    //Inca nu bagam sunete in joc, dar nu strica sa fie facut scriptul
+    public AudioSource proba;
+	void Start () 
+    {
+        proba.time = 20;
+        proba.pitch = 0.7f;
+	}
+	void Update () 
+    {
+        
+	}
+}
