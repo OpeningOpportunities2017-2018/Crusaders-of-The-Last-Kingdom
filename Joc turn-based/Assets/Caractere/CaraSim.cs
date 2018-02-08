7380cbebb6b3de66c55ba90d6ea355701ab79ebb
Cosmin Aftanase <cosmin.aftanase@yahoo.com>
2018-02-08 21:02:40 +0200

    Exemplu de schimbari de animatii

---
+++ Joc turn-based/Assets/Caractere/CaraSim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaraSim : MonoBehaviour {
+    CombatSimulator cs;
+	// Use this for initialization
+	void Start () {
+        cs = FindObjectOfType<CombatSimulator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        Debug.Log("test");
+        if (cs.g1 == null)
+        { cs.g1 = this.gameObject; cs.retine1 = this.transform.position; }
+        else if (cs.g1 != null && cs.g2 == null)
+        { cs.g2 = this.gameObject; cs.retine2 = this.transform.position; }
+    }
+}
---
+++ Joc turn-based/Assets/Caractere/CombatSimulator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class CombatSimulator : MonoBehaviour {
+    public GameObject g1, g2;
+    public GameObject poz1, poz2;
+    public Vector3 retine1, retine2;
+    bool ok;
+    [Range(0.0f,2f)]
+    public float step;
+	// Use this for initialization
+	void Start () {
+        ok = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(g1!=null && g2!=null && ok==true)
+        {
+            g1.transform.position = Vector3.MoveTowards(g1.transform.position, poz1.transform.position, step);
+           // g1.transform.position = poz1.transform.position;
+          //  g2.transform.position = poz2.transform.position;
+            g2.transform.position = Vector3.MoveTowards(g2.transform.position, poz2.transform.position, step);
+            if (g1.transform.position == poz1.transform.position && g2.transform.position == poz2.transform.position)
+            { ok = false; StartCoroutine(anim()); }
+       }
+	}
+    public void resetare()
+    {
+        Debug.Log("resetare");
+        g1.transform.position = retine1;
+        g2.transform.position = retine2;
+        StopAllCoroutines();
+        g1.GetComponent<UnityArmatureComponent>().animation.Play("Idle");
+        g2.GetComponent<UnityArmatureComponent>().animation.Play("Idle");
+        g1 = null;
+        g2 = null;
+        ok = true;
+    }
+    IEnumerator anim()
+    {
+        g1.GetComponent<UnityArmatureComponent>().animation.Play("Attack");
+        yield return new WaitForSeconds(0.5f);
+        g2.GetComponent<UnityArmatureComponent>().animation.Play("Damaged");
+    }
+}
