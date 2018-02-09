38b550c855ca28ad5e2c425878f4716927a903b1
Cosmin Aftanase <cosmin.aftanase@yahoo.com>
2018-02-10 00:56:51 +0200

    Curs :o: r  :open_mouth:
    :b:

---
+++ Joc turn-based/Assets/Scripturi/CursorCosmin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorCosmin : MonoBehaviour
+{
+
+    public Texture2D cursorTexture, cursorTextureClicked;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Awake()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        DontDestroyOnLoad(this.gameObject);
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            Cursor.SetCursor(cursorTextureClicked, hotSpot, cursorMode);
+        else
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    } 
+}
